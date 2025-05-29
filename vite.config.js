f5d33535f339da56d8cc8c9c47d839a11c6f65a6
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-05-29 19:18:59 +0300

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/A-Portfolio-Website/",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
